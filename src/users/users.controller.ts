6bd813439b26c6d72d0957805747e35d6207de6f
Wei Kian <weikian92@hotmail.com>
2024-05-29 18:25:29 +0800

    add create user api, update user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Body, ValidationPipe, ParseIntPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,11 +10,21 @@ export class UsersController {
 
     @Get() // GET /users
     getUser() {
+        return this.usersService.findAll();
+    }
 
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id);
     }
 
     @Post() // POST /users
     createNewUser(@Body(ValidationPipe) createUserDto: CreateUserDto) {
         return this.usersService.createUser(createUserDto);
+    }
+
+    @Put(':id') // PUT /user/id
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.update(id, updateUserDto);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/db/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
+    private saltRounds = 10;
+
     constructor(
         @InjectRepository(User)
-        private usersRepository: Repository<User>,
+        private usersRepository: Repository<User>
     ) { }
 
-    findAll(): Promise<User[]> {
-        return this.usersRepository.find();
+    async getLastUserId(): Promise<number> {
+        const lastUser = await this.usersRepository.find({
+            order: { id: 'DESC' },
+            take: 1
+        });
+        return lastUser[0] ? lastUser[0].id : 0; // Return 0 if no users exist
     }
 
-    findOne(id: number): Promise<User | null> {
-        return this.usersRepository.findOneBy({ id });
+    async findAll(): Promise<User[]> {
+        return await this.usersRepository.find();
     }
 
-    createUser(createUserDto: CreateUserDto) {
-        const username = createUserDto.fullName;
+    async findOne(id: number): Promise<User | null> {
+        const user = await this.usersRepository.findOneBy({ id });
+
+        if (!user) {
+            throw new NotFoundException('User Not Found');
+        }
+        return user;
+    }
+
+    async createUser(createUserDto: CreateUserDto) {
         const role = 'student';
         const takenQuizes = [];
-        const newUser = {
-            ...createUserDto, username, role, takenQuizes
-        };
+        const passwordHash = await bcrypt.hash(createUserDto.password, this.saltRounds);
+        const username = this.generateUniqueUsername(createUserDto.fullName, await this.getLastUserId());
 
-        return this.usersRepository.save(newUser);
+        const response = await this.usersRepository.save({
+            ...createUserDto, username, role, takenQuizes, password: passwordHash
+        });
+        return response;
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto) {
+        const user = await this.findOne(id);
+        if (user) {
+            const passwordHash = await bcrypt.hash(updateUserDto.password, this.saltRounds);
+            return this.usersRepository.update(id, {...updateUserDto, password: passwordHash});
+        }
+    }
+
+    private generateUniqueUsername(fullname: string, id: number): string {
+        const nameWithoutSpaces = fullname.replace(/\s+/g, '').toLowerCase();
+        return `${nameWithoutSpaces}-${(id+1)}`;
     }
 }
